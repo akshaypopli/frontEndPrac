3452395d2fcff35f05ec152b15d69cf6969ad776
Mac <mac@Akshays-Air.local>
2020-08-24 08:16:32 -0400

    external script file

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function toggleSearchBar(){
+    if(document.getElementById("search-container").style.display == "block"){
+        document.getElementById("search-container").style.display = "none";
+    }else {
+        document.getElementById("search-container").style.display = "block";
+    }
+}
